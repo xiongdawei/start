b745199337c0142e43ddb9542ff95bf2b0159686
Paddingtondx <42790040+xiongdawei@users.noreply.github.com>
2018-11-12 08:04:08 +0800

    Update stack.py

--- Homework/stack.py
+++ Homework/stack.py
@@ -1,24 +1,32 @@
-class stack():
+class Stack:
     def __init__(self):
-        self.stack1=[]
-    def isempty(self):
-        if len(self.stack1)==0:
-            return True
-        else:
-            return len(self.stack1)
-    def push(self,a):
-        self.stack1.append(a)
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    def push(self,item):
+        self.items.append(item)
+        
     def pop(self):
-        self.stack1.remove(self.stack1[0])
-    def read(self):
-        return self.stack1
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items)-1]
+    
+    def size(self):
+        return len(self.items)
+    
+s = Stack()
 
-
-test=stack()
-test.push(1)
-test.push(2)
-test.pop()
-print(test.read())
-print(test.isempty())
-test.pop()
-print(test.isempty())
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
